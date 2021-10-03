54a4c93c5e1dcb49df11c373e323e44d15714994
Kushal Das <mail@kushaldas.in>
2021-10-03 18:16:45 +0530

    Updates setup

--- setup.py
+++ setup.py
@@ -1,8 +1,23 @@
 from setuptools import setup
 
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="amrasaman",
-    version="0.1.0",
+    version="0.1.1",
+    author="Kushal Das",
+    author_email="mail@kushaldas.in",
+    description="A tool to build reproducible wheels.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kushaldas/amrasaman",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     py_modules=["amrasaman"],
     install_requires=[
         "Click",
