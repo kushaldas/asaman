95f8fb8d6d3ae566d9032d2d775a52ca87020533
Kushal Das <mail@kushaldas.in>
2021-10-03 21:24:40 +0530

    Renames to asaman

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="amrasaman",
+    name="asaman",
     version="0.1.5",
     author="Kushal Das",
     author_email="mail@kushaldas.in",
     description="A tool to build reproducible wheels.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kushaldas/amrasaman",
+    url="https://github.com/kushaldas/asaman",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
